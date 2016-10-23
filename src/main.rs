fcb50c5d404db44dcddc9568b22b7b0143c3df3e
Maxime Vaude <maxime.vaude@gmail.com>
2016-10-24 00:13:45 +0200

    feature(main): print all asciiart from asciifart.
    It is tryinh all possible urls to print ascii art.

--- src/main.rs
+++ src/main.rs
@@ -13,16 +13,26 @@ fn get_art(url: String) -> String {
 
     let res = client.get(&url)
         .send().unwrap();
-    assert_eq!(res.status, hyper::Ok);
+    if res.status == hyper::NotFound {
+        return "nothing".to_string();
+    }
     let document = kuchiki::parse_html().from_http(res).unwrap();
 
     return document.select("pre").unwrap().last().unwrap().text_contents();
 }
 
 fn main() {
-    let url = "http://www.asciiartfarts.com/19990625.html";
-    let art = get_art(url.to_string());
-    println!("{}", art);
+    let url = "http://www.asciiartfarts.com/";
+    for year in 1999..2015 {
+        for month in 1..13 {
+            for day in 1..32 {
+                let art_url = url.to_string() + &year.to_string() + &format!("{:02}", month) + &format!("{:02}", day) + &".html".to_string();
+                println!("{}", art_url);
+                let art = get_art(art_url);
+                println!("{}", art);
+            }
+        }
+    }
     // println!("Response: {}", res.status);
     // println!("Headers:\n{}", res.headers);
     println!("everything went fine");
