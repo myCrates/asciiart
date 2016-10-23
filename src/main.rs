10b23e1ec0caa03f61635cb44e879ecfdc8d6d27
Maxime Vaude <maxime.vaude@gmail.com>
2016-10-23 23:24:56 +0200

    refactor(main): clean existing code:
    - add function to get ascii art.
    - use different kuchiki method to get ascii art.

--- src/main.rs
+++ src/main.rs
@@ -5,25 +5,24 @@
 extern crate hyper;
 extern crate kuchiki;
 
-use std::io;
-
 use hyper::client::Client;
-use kuchiki::NodeRef;
 use kuchiki::traits::ParserExt;
 
-fn main() {
+fn get_art(url: String) -> String {
     let client = Client::new();
 
-    let res = client.get("http://www.asciiartfarts.com/19990625.html")
+    let res = client.get(&url)
         .send().unwrap();
+    assert_eq!(res.status, hyper::Ok);
     let document = kuchiki::parse_html().from_http(res).unwrap();
 
-    for node in document.select("pre") {
-        for child in node {
-            println!("{}", child.text_contents());
-        }
-    }
+    return document.select("pre").unwrap().last().unwrap().text_contents();
+}
 
+fn main() {
+    let url = "http://www.asciiartfarts.com/19990625.html";
+    let art = get_art(url.to_string());
+    println!("{}", art);
     // println!("Response: {}", res.status);
     // println!("Headers:\n{}", res.headers);
     println!("everything went fine");
