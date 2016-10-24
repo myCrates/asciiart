d7e64689bddc46604c40d4713ef26429b1ebcfc8
Maxime Vaude <maxime.vaude@gmail.com>
2016-10-25 01:58:45 +0200

    feat(fortune): get random ascii art.
    Main feature is now working.
    Some refactoring, test and documentation are needed.

--- src/main.rs
+++ src/main.rs
@@ -2,34 +2,40 @@
 //   - init phase (downloading asciiarts).
 //   - main phase (select the asciiart to output
 
-extern crate hyper;
+extern crate rand; // https://crates.io/crates/rand
 
-use std::process::Command;
+use rand::{Rng};         // The generic trait all random generators support.
+use rand::os::{OsRng};   // Specific implementation of above for strong crypto.
+
 use std::fs::File;
-use std::io::{Read, Write, BufWriter};
+use std::io::{Read, BufReader};
+use std::path::PathBuf;
 
-use hyper::client::Client;
-
-fn get_art(url: String) -> String {
-    let client = Client::new();
-
+fn get_art() -> String {
     let mut output = String::new();
-    let mut res = client.get(&url).send().unwrap();
-
-    res.read_to_string(&mut output).expect("no response body");
-    let pwd = Command::new("sh").arg("-c").arg("pwd").output().expect("Failed to pwd");
-    println!("{}", String::from_utf8_lossy(&pwd.stdout));
-    let f = File::create("assets/asciiartfarts.com").expect("Unable to create file");
-    let mut f = BufWriter::new(f);
-    f.write_all(output.as_bytes()).expect("Unable to write data");
+    let rp = PathBuf::from("/var/lib/assets/asciiartfarts.txt");
+    let f = File::open(rp.to_str().unwrap()).expect("couldn't open file");
+    let mut br = BufReader::new(f);
+    br.read_to_string(&mut output).expect("Unable to read string");
     return output;
 }
 
 fn main() {
-    let url = "http://www.asciiartfarts.com/fortune.txt";
-    let art = get_art(url.to_string());
-    println!("{}", art);
+    let art = get_art();
+    // OsRng is a type of `Rng` that wraps /dev/urandom, getrandom(), etc.
+    let mut r = OsRng::new().unwrap();
+
+    // Random bytes.
+    let mut my_secure_bytes = vec![0u8; 1500];
+    r.fill_bytes(&mut my_secure_bytes);
+
+    // Primitive types and short arrays.
+    let my_secure_int: usize = r.gen_range(0, 5372);
+
+    // println!("First few bytes = {:?}; random int = {:?}",
+    //     &my_secure_bytes[..5], my_secure_int);
+    println!("{}", art.split("\n%\n").nth(my_secure_int).unwrap());
     // println!("Response: {}", res.status);
     // println!("Headers:\n{}", res.headers);
-    println!("everything went fine");
+    // println!("everything went fine");
 }
